54a0bfad0f20e24b2f0002fb2df1d9479a47ea2d
joel burton <joeltburton@gmail.com>
2019-02-13 11:16:39 -0800

    improve configuration in a couple small ways
    - removed redundant call to dotenv from db.js.
      Only need the one call in server/index.js
    
    - imported stylesheet to client index
      Allows styles to be detected for hot reloading
    
    - Added .DS_Store to .gitignore
      It's a system dependent that's automatically generated by OSX

--- demo/client/index.js
+++ demo/client/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '../dist/style.css';
 import App from './App';
+
 
 ReactDOM.render(
   <App />,
--- demo/server/Models/db.js
+++ demo/server/Models/db.js
@@ -1,5 +1,5 @@
-require('dotenv').config();
 const pgp = require('pg-promise')();
+
 
 const db = pgp(process.env.DB_URI);
 
