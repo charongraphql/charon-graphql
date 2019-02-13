3a645df6d0a7f9ef894e29a2d2e04cef80c1da3d
joel burton <joeltburton@gmail.com>
2019-02-13 15:46:31 -0800

    build out demoTools

--- demo/client/App.jsx
+++ demo/client/App.jsx
@@ -4,12 +4,11 @@ import DemoContainer from './components/DemoContainer';
 const App = () => (
   <div className="app">
     <nav>
+      <span>demo 1</span>
       {' '}
-      <span>demo 1</span> 
-{' '}
-<span>demo 2</span> 
-{' '}
-<span>demo 3</span>
+      <span>demo 2</span>
+      {' '}
+      <span>demo 3</span>
     </nav>
 
     <DemoContainer />
--- demo/client/components/DemoTools.jsx
+++ demo/client/components/DemoTools.jsx
@@ -1,8 +1,60 @@
 import React, { useState, useEffect } from 'react';
 
-const DemoTools = () => (
+import StateDisplay from './StateDisplay';
+
+const stateData = {
+  number: 5182836,
+  string: '٩(｡•́‿•̀｡)۶',
+  boolean: true,
+  'undefined val': undefined,
+  'null val': null,
+
+
+  array: [true, null, 12345, 'wow'],
+  object: {
+    nested: {
+      key: {
+        value: {
+          pairs: '( ´_ゝ`)',
+        },
+      },
+    },
+  },
+};
+
+const cacheData = {
+  'Post:5': {
+    id: 5,
+    some: 'data',
+    about: 'a',
+    neat: 'book',
+  },
+  'Post:7': {
+    id: 7,
+    here: 'is',
+    more: 'data',
+    about: 'a',
+    post: 'wow',
+  },
+  'Post:14': {
+    id: 14,
+    some: 'fake',
+    stuff: 'here',
+    too: '...',
+  },
+  'Post:3': {
+    id: 3,
+    some: 'data',
+    about: 'a',
+    neat: 'book',
+  },
+};
+
+const DemoTools = props => (
   <div className="demo-tools">
-    This is the Demo Tools
+    <StateDisplay title="State" data={stateData} />
+    <StateDisplay title="Cache" data={cacheData} />
+    <StateDisplay title="Queries" data={stateData} />
   </div>
 );
 
