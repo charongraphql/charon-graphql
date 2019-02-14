16f7ae4ec6361b14da3f336ab98b68cbfbe5316c
joel burton <joeltburton@gmail.com>
2019-02-13 23:00:39 -0800

    styling for demo-app

--- demo/client/components/StateDisplay.jsx
+++ demo/client/components/StateDisplay.jsx
@@ -2,15 +2,23 @@ import React from 'react';
 import JSONTree from 'react-json-tree';
 import ReactJson from 'react-json-view';
 
+const jsonStyle = {
+  // borderColor: 'white',
+  // borderStyle: 'solid',
+  backgroundColor: 'rgba(0, 0, 0, 0.25)',
+  borderRadius: '5px',
+  padding: '3px 10px',
+};
 
 const StateDisplay = ({ data, title }) => (
   <div className="state-display">
     <h2>{title}</h2>
-    <div className="json-view">
+    <div className="json-container">
       <ReactJson
+        style={jsonStyle}
         src={data}
         name={null}
-        theme="summerfruit:inverted"
+        theme="summerfruit"
         iconStyle="triangle"
         indentWidth={2}
         collapsed={1}
