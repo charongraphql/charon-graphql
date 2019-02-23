378b3184d11bbec11c0b2902017eea41a3f76ce6
Chang Yea Moon <changyeamoon@gmail.com>
2019-02-22 20:12:56 -0800

    test ListingCreator with snapshots

--- demo/__tests__/components/demo_one_test/ListingCreator.spec.js
+++ demo/__tests__/components/demo_one_test/ListingCreator.spec.js
@@ -1,19 +1,73 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
-// import { MockedProvider } from 'react-apollo/test-utils'; cant find lib for
+import { MockedProvider } from 'react-apollo/test-utils';
+import { render, wait, cleanup } from 'react-testing-library';
+import toJSON from 'enzyme-to-json';
 import ListingCreator from '../../../client/components/demo_one/ListingCreator';
+import {
+  getAuthorsQuery,
+  addListingMutation,
+  getListingsQuery,
+} from '../../../client/queries/queries';
 
 describe('ListingCreator', () => {
-  test('should render a <div />', () => {
-    const wrapper = shallow(<ListingCreator />);
-    expect(wrapper.find('div')).toHaveLength(0);
+  let wrapper;
+  const mocks = [
+    {
+      request: {
+        query: getAuthorsQuery,
+      },
+      result: {
+        data: {
+          authors: { id: '1', name: 's' },
+        },
+      },
+    },
+  ];
+  // beforeEach(() => {
+  //   wrapper = shallow(
+  //     <MockedProvider mocks={mocks} addTypename={false}>
+  //       <ListingCreator />
+  //     </MockedProvider>,
+  //   );
+  // });
+
+  // afterEach(() => {
+  //   expect(wrapper).toMatchSnapshot();
+  // });
+
+  test('should render loading state initially', () => {
+    const { container } = render(
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <ListingCreator />
+      </MockedProvider>,
+    );
+    expect(container).toMatchSnapshot();
+    // this way does work with enzyme mount, but could get data to render
+    // expect(toJSON(wrapper.find('ListingCreator'))).toMatchSnapshot();
   });
 
-  test('sets title state to input value on change', () => {
-    const wrapper = shallow(<ListingCreator />);
-    console.log(wrapper.debug());
-    wrapper.find('div').simulate('change', { target: { value: 'chang' } });
-    expect(2).toBe(2);
-    wrapper.unmount();
+  test('renders data', async () => {
+    const { queryByText, container } = render(
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <ListingCreator />
+      </MockedProvider>,
+    );
+    console.log(queryByText);
+    await waitUntilLoadingIsFinished(queryByText);
+
+    expect(container).toMatchSnapshot();
   });
+
+  // test.skip('should render authors', () => {
+  //   // console.log(wrapper.debug());
+  //   wrapper.find('select').simulate('change', { target: { value: 'chang' } });
+  //   console.log(wrapper);
+  //   expect(
+  //     wrapper
+  //       .find('select')
+  //       .props()
+  //       .value(),
+  //   ).toBe('chang');
+  // });
 });
