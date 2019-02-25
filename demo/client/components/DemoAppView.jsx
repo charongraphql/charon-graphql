1a0387ecfbab6a683dd39db692918cbcfba77680
Chang Yea Moon <changyeamoon@gmail.com>
2019-02-24 22:36:47 -0800

    create demo using vanilla GQL

--- demo/client/components/DemoAppView.jsx
+++ demo/client/components/DemoAppView.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 import CharonsList from './demo_one/CharonsList';
+// Apollo Client Setup
+const client = new ApolloClient({
+  // server endpoint
+  uri: 'http://localhost:4000/graphql',
+});
 
 const DemoAppView = ({ match }) => {
   const selectDemo = () => {
     if (match.params.demo === 'demo1') {
-      return <CharonsList />;
+      return (
+        <ApolloProvider client={client}>
+          <CharonsList apollo />;
+        </ApolloProvider>
+      );
     }
     if (match.params.demo === 'demo2') {
-      return <CharonsList />;
+      return <CharonsList apollo={false} />;
     }
     if (match.params.demo === 'demo3') {
       return <CharonsList />;
